5811b6069369989d679e56e019d216fcade8e924
craigmc08 <craigmc08@gmail.com>
2018-12-30 13:17:45 -0500

    Refactor tests

--- test/index.test.js
+++ test/index.test.js
@@ -24,154 +24,160 @@ const {
 
 const { matches, not } = predicate;
 
-test('default everything should catch everything', () => {
-    const gobble = Tokenizer().default(everything(makeToken('EVERYTHING')));
+describe('Controllers', () => {
+    test('default everything should catch everything', () => {
+        const gobble = Tokenizer().default(everything(makeToken('EVERYTHING')));
 
-    const text = 'the quick brown fox';
+        const text = 'the quick brown fox';
 
-    expect(gobble(text)).toEqual([CreateToken(text)('EVERYTHING')(text, 0)]);
+        expect(gobble(text)).toEqual([CreateToken(text)('EVERYTHING')(text, 0)]);
+    });
+
+    test('onChar should run reducers if the first char matches', () => {
+        const morseCode = Tokenizer()
+            .onChar({
+                '.': single(makeToken('DOT')),
+                '_': single(makeToken('DASH')),
+            })
+        ;
+
+        const text = '._..';
+
+        const dot = i => CreateToken(text)('DOT')('.', i);
+        const dash = i => CreateToken(text)('DASH')('_', i);
+
+        const expected = [
+            dot(0), dash(1), dot(2), dot(3),
+        ];
+
+        expect(morseCode(text)).toEqual(expected);
+    });
+
+    test('if should run the given reducer if the predicate is true', () => {
+        const isVowel = matches(/[aeiou]/);
+        const isConsonant = not(isVowel);
+
+        const wordCategorizer = Tokenizer()
+            .if(isVowel, everythingUntil(' ')(makeToken('vowelWord')))
+            .if(isConsonant, everythingUntil(' ')(makeToken('consonantWord')))
+        ;
+
+        const text = 'horse apple fly ill';
+
+        const vWord = CreateToken(text)('vowelWord');
+        const cWord = CreateToken(text)('consonantWord');
+
+        expected = [ cWord('horse', 0), vWord('apple', 6), cWord('fly', 12), vWord('ill', 16) ];
+
+        expect(wordCategorizer(text)).toEqual(expected);
+    });
+
+    test('keywords should find the listed keywords', () => {
+        const keywordExtractor = Tokenizer()
+            .keywords({
+                'and': makeToken('and'),
+                'or': makeToken('or'),
+                'not': makeToken('not'),
+            })
+            .default(everythingUntil(' ')(makeToken('text')))
+        ;
+
+        const text = 'horses and not butterflies or beesnot';
+
+        const makeAnd = o => CreateToken(text)('and')('and', o);
+        const makeOr = o => CreateToken(text)('or')('or', o);
+        const makeNot = o => CreateToken(text)('not')('not', o);
+        const makeText = CreateToken(text)('text');
+
+        const expected = [
+            makeText('horses', 0), makeAnd(7), makeNot(11),
+            makeText('butterflies', 15), makeOr(27), makeText('beesnot', 30),
+        ];
+
+        expect(keywordExtractor(text)).toEqual(expected);
+    });
 });
 
-test('onChar should run reducers if the first char matches', () => {
-    const morseCode = Tokenizer()
-        .onChar({
-            '.': single(makeToken('DOT')),
-            '_': single(makeToken('DASH')),
-        })
-    ;
+describe('Reducers', () => {
+    test('everythingUntil should add characters until it reaches an invalid character', () => {
+        const csv = Tokenizer()
+            .onChar({
+                ',': single(makeToken('COMMA')),
+                '\n': single(makeToken('NEWLINE')),
+            })
+            .default(everythingUntil(',', '\n')(makeToken('VALUE')))
+        ;
 
-    const text = '._..';
+        const text = '1,Up\n2,Left\n3,Right';
 
-    const dot = i => CreateToken(text)('DOT')('.', i);
-    const dash = i => CreateToken(text)('DASH')('_', i);
+        const comma = i => CreateToken(text)('COMMA')(',', i);
+        const newline = i => CreateToken(text)('NEWLINE')('\n', i);
+        const value = CreateToken(text)('VALUE');
 
-    const expected = [
-        dot(0), dash(1), dot(2), dot(3),
-    ];
+        const expected = [
+            value('1', 0), comma(1), value('Up', 2), newline(4),
+            value('2', 5), comma(6), value('Left', 7), newline(11),
+            value('3', 12), comma(13), value('Right', 14),
+        ];
 
-    expect(morseCode(text)).toEqual(expected);
+        expect(csv(text)).toEqual(expected);
+    });
+
+    test('sequence should consume sequences of consumers', () => {
+        const stringTokenizer = Tokenizer()
+            .onChar({
+                '"': sequence(
+                    char('"'),
+                    untilRegexFails(/^(\\"|[^\n"])*$/),
+                    char('"'),
+                )(makeToken('STRING')),
+            })
+        ;
+
+        const text = '"this is a string" these won\'t show up. "another string"';
+
+        const expected = [
+            CreateToken(text)('STRING')('"this is a string"', 0),
+            CreateToken(text)('STRING')('"another string"', 40),
+        ];
+
+        expect(stringTokenizer(text)).toEqual(expected);
+    });
+
+    test('consume should consume characters from one consumer and create a token', () => {
+        const consumeTest = Tokenizer()
+            .default(consume(whitespace())(makeToken('WHITESPACE')))
+        ;
+
+        const text = '    \nepic\t   ';
+
+        const expected = [
+            CreateToken(text)('WHITESPACE')('    \n', 0),
+            CreateToken(text)('WHITESPACE')('\t   ', 9),
+        ];
+
+        expect(consumeTest(text)).toEqual(expected);
+    });
 });
 
-test('everythingUntil should add characters until it reaches an invalid character', () => {
-    const csv = Tokenizer()
-        .onChar({
-            ',': single(makeToken('COMMA')),
-            '\n': single(makeToken('NEWLINE')),
-        })
-        .default(everythingUntil(',', '\n')(makeToken('VALUE')))
-    ;
+describe('Miscellaneous functions', () => {
+    test('prettyPrint should format the tokens nicely for viewing', () => {
+        const csv = Tokenizer()
+            .onChar({
+                ',': single(makeToken('COMMA')),
+                '\n': single(makeToken('NEWLINE')),
+            })
+            .default(everythingUntil(',', '\n')(makeToken('VALUE')))
+        ;
 
-    const text = '1,Up\n2,Left\n3,Right';
+        const text = '1,Up\n2,Left\n3,Right';
 
-    const comma = i => CreateToken(text)('COMMA')(',', i);
-    const newline = i => CreateToken(text)('NEWLINE')('\n', i);
-    const value = CreateToken(text)('VALUE');
+        const comma = i => CreateToken(text)('COMMA')(',', i);
+        const newline = i => CreateToken(text)('NEWLINE')('\n', i);
+        const value = CreateToken(text)('VALUE');
 
-    const expected = [
-        value('1', 0), comma(1), value('Up', 2), newline(4),
-        value('2', 5), comma(6), value('Left', 7), newline(11),
-        value('3', 12), comma(13), value('Right', 14),
-    ];
+        expected = `[\n  <Token type='VALUE' lexeme='1' offset=0>,\n  <Token type='COMMA' lexeme=',' offset=1>,\n  <Token type='VALUE' lexeme='Up' offset=2>,\n  <Token type='NEWLINE' lexeme='\n' offset=4>,\n  <Token type='VALUE' lexeme='2' offset=5>,\n  <Token type='COMMA' lexeme=',' offset=6>,\n  <Token type='VALUE' lexeme='Left' offset=7>,\n  <Token type='NEWLINE' lexeme='\n' offset=11>,\n  <Token type='VALUE' lexeme='3' offset=12>,\n  <Token type='COMMA' lexeme=',' offset=13>,\n  <Token type='VALUE' lexeme='Right' offset=14>\n]`;
 
-    expect(csv(text)).toEqual(expected);
-});
-
-test('sequence should consume sequences of consumers', () => {
-    const stringTokenizer = Tokenizer()
-        .onChar({
-            '"': sequence(
-                char('"'),
-                untilRegexFails(/^(\\"|[^\n"])*$/),
-                char('"'),
-            )(makeToken('STRING')),
-        })
-    ;
-
-    const text = '"this is a string" these won\'t show up. "another string"';
-
-    const expected = [
-        CreateToken(text)('STRING')('"this is a string"', 0),
-        CreateToken(text)('STRING')('"another string"', 40),
-    ];
-
-    expect(stringTokenizer(text)).toEqual(expected);
-});
-
-test('consume should consume characters from one consumer and create a token', () => {
-    const consumeTest = Tokenizer()
-        .default(consume(whitespace())(makeToken('WHITESPACE')))
-    ;
-
-    const text = '    \nepic\t   ';
-
-    const expected = [
-        CreateToken(text)('WHITESPACE')('    \n', 0),
-        CreateToken(text)('WHITESPACE')('\t   ', 9),
-    ];
-
-    expect(consumeTest(text)).toEqual(expected);
-});
-
-test('if should run the given reducer if the predicate is true', () => {
-    const isVowel = matches(/[aeiou]/);
-    const isConsonant = not(isVowel);
-
-    const wordCategorizer = Tokenizer()
-        .if(isVowel, everythingUntil(' ')(makeToken('vowelWord')))
-        .if(isConsonant, everythingUntil(' ')(makeToken('consonantWord')))
-    ;
-
-    const text = 'horse apple fly ill';
-
-    const vWord = CreateToken(text)('vowelWord');
-    const cWord = CreateToken(text)('consonantWord');
-
-    expected = [ cWord('horse', 0), vWord('apple', 6), cWord('fly', 12), vWord('ill', 16) ];
-
-    expect(wordCategorizer(text)).toEqual(expected);
-});
-
-test('keywords should find the listed keywords', () => {
-    const keywordExtractor = Tokenizer()
-        .keywords({
-            'and': makeToken('and'),
-            'or': makeToken('or'),
-            'not': makeToken('not'),
-        })
-        .default(everythingUntil(' ')(makeToken('text')))
-    ;
-
-    const text = 'horses and not butterflies or beesnot';
-
-    const makeAnd = o => CreateToken(text)('and')('and', o);
-    const makeOr = o => CreateToken(text)('or')('or', o);
-    const makeNot = o => CreateToken(text)('not')('not', o);
-    const makeText = CreateToken(text)('text');
-
-    const expected = [
-        makeText('horses', 0), makeAnd(7), makeNot(11),
-        makeText('butterflies', 15), makeOr(27), makeText('beesnot', 30),
-    ];
-
-    expect(keywordExtractor(text)).toEqual(expected);
-});
-
-test('prettyPrint should format the tokens nicely for viewing', () => {
-    const csv = Tokenizer()
-        .onChar({
-            ',': single(makeToken('COMMA')),
-            '\n': single(makeToken('NEWLINE')),
-        })
-        .default(everythingUntil(',', '\n')(makeToken('VALUE')))
-    ;
-
-    const text = '1,Up\n2,Left\n3,Right';
-
-    const comma = i => CreateToken(text)('COMMA')(',', i);
-    const newline = i => CreateToken(text)('NEWLINE')('\n', i);
-    const value = CreateToken(text)('VALUE');
-
-    expected = `[\n  <Token type='VALUE' lexeme='1' offset=0>,\n  <Token type='COMMA' lexeme=',' offset=1>,\n  <Token type='VALUE' lexeme='Up' offset=2>,\n  <Token type='NEWLINE' lexeme='\n' offset=4>,\n  <Token type='VALUE' lexeme='2' offset=5>,\n  <Token type='COMMA' lexeme=',' offset=6>,\n  <Token type='VALUE' lexeme='Left' offset=7>,\n  <Token type='NEWLINE' lexeme='\n' offset=11>,\n  <Token type='VALUE' lexeme='3' offset=12>,\n  <Token type='COMMA' lexeme=',' offset=13>,\n  <Token type='VALUE' lexeme='Right' offset=14>\n]`;
-
-    expect(prettyPrint(csv(text))).toBe(expected);
+        expect(prettyPrint(csv(text))).toBe(expected);
+    });
 });
--- test/predicate.test.js
+++ test/predicate.test.js
@@ -3,85 +3,87 @@ const {
     not, or, nor, and, nand, xor
 } = require('../src/predicate');
 
-test('is should return true for values that are the same', () => {
-    expect(is('a')('a')).toBe(true);
-    expect(is('a')('b')).toBe(false);
+describe('Predicates', () => {
+    test('is should return true for values that are the same', () => {
+        expect(is('a')('a')).toBe(true);
+        expect(is('a')('b')).toBe(false);
 
-    expect(is({a: 1})({a: 1})).toBe(false); // Does not compare object structure
-    const testObj = {a: 1};
-    expect(is(testObj)(testObj)).toBe(true); // Does compare object reference
+        expect(is({a: 1})({a: 1})).toBe(false); // Does not compare object structure
+        const testObj = {a: 1};
+        expect(is(testObj)(testObj)).toBe(true); // Does compare object reference
+    });
+
+    test('isOneOf should return true when the actual matches any truth', () => {
+        const isFruit = isOneOf('apple', 'orange', 'tomato');
+        const isFactorOf6 = isOneOf(1, 2, 3, 6);
+
+        expect(isFruit('apple')).toBe(true);
+        expect(isFruit('celery')).toBe(false);
+
+        expect(isFactorOf6(3)).toBe(true);
+        expect(isFactorOf6(4)).toBe(false);
+    });
+
+    test('matches should return true when the actual matches the regex', () => {
+        const isNumber = matches(/^-?([1-9][0-9]*|0)(\.[0-9]+)?([eE][+-]?[0-9]+)?$/);
+
+        expect(isNumber('-15.3')).toBe(true);
+        expect(isNumber('01')).toBe(false);
+        expect(isNumber('315.96E-20')).toBe(true);
+    });
+
+    test('not should negate value of predicate', () => {
+        expect(not(is('apple'))('orange')).toBe(true);
+        expect(not(is('apple'))('apple')).toBe(false);
+    });
+
+    test('or should return true when any of values are true', () => {
+        // starts with consonant or ends with vowel
+        const matchesCriteria = or(matches(/^[^aeiou]/), matches(/[aeiou]$/));
+
+        expect(matchesCriteria('wore')).toBe(true);
+        expect(matchesCriteria('apple')).toBe(true);
+        expect(matchesCriteria('toast')).toBe(true);
+        expect(matchesCriteria('our')).toBe(false);
+    });
+
+    test('nor should return false when any values are true', () => {
+        // starts with consonant or ends with vowel
+        const matchesCriteria = nor(matches(/^[^aeiou]/), matches(/[aeiou]$/));
+
+        expect(matchesCriteria('wore')).toBe(false);
+        expect(matchesCriteria('apple')).toBe(false);
+        expect(matchesCriteria('toast')).toBe(false);
+        expect(matchesCriteria('our')).toBe(true);
+    });
+
+    test('and should return true when both values are true', () => {
+        // starts with consonant and ends with vowel
+        const matchesCriteria = and(matches(/^[^aeiou]/), matches(/[aeiou]$/));
+
+        expect(matchesCriteria('wore')).toBe(true);
+        expect(matchesCriteria('apple')).toBe(false);
+        expect(matchesCriteria('toast')).toBe(false);
+        expect(matchesCriteria('our')).toBe(false);
+    });
+
+    test('nand should return false when both values are true', () => {
+        // starts with consonant and ends with vowel
+        const matchesCriteria = nand(matches(/^[^aeiou]/), matches(/[aeiou]$/));
+
+        expect(matchesCriteria('wore')).toBe(false);
+        expect(matchesCriteria('apple')).toBe(true);
+        expect(matchesCriteria('toast')).toBe(true);
+        expect(matchesCriteria('our')).toBe(true);
+    });
+
+    test('xor should only return true when one value is true', () => {
+        // starts with consonant and ends with vowel
+        const matchesCriteria = xor(matches(/^[^aeiou]/), matches(/[aeiou]$/));
+
+        expect(matchesCriteria('wore')).toBe(false);
+        expect(matchesCriteria('apple')).toBe(true);
+        expect(matchesCriteria('toast')).toBe(true);
+        expect(matchesCriteria('our')).toBe(false);
+    });
 });
-
-test('isOneOf should return true when the actual matches any truth', () => {
-    const isFruit = isOneOf('apple', 'orange', 'tomato');
-    const isFactorOf6 = isOneOf(1, 2, 3, 6);
-
-    expect(isFruit('apple')).toBe(true);
-    expect(isFruit('celery')).toBe(false);
-
-    expect(isFactorOf6(3)).toBe(true);
-    expect(isFactorOf6(4)).toBe(false);
-});
-
-test('matches should return true when the actual matches the regex', () => {
-    const isNumber = matches(/^-?([1-9][0-9]*|0)(\.[0-9]+)?([eE][+-]?[0-9]+)?$/);
-
-    expect(isNumber('-15.3')).toBe(true);
-    expect(isNumber('01')).toBe(false);
-    expect(isNumber('315.96E-20')).toBe(true);
-});
-
-test('not should negate value of predicate', () => {
-    expect(not(is('apple'))('orange')).toBe(true);
-    expect(not(is('apple'))('apple')).toBe(false);
-});
-
-test('or should return true when any of values are true', () => {
-    // starts with consonant or ends with vowel
-    const matchesCriteria = or(matches(/^[^aeiou]/), matches(/[aeiou]$/));
-
-    expect(matchesCriteria('wore')).toBe(true);
-    expect(matchesCriteria('apple')).toBe(true);
-    expect(matchesCriteria('toast')).toBe(true);
-    expect(matchesCriteria('our')).toBe(false);
-});
-
-test('nor should return false when any values are true', () => {
-    // starts with consonant or ends with vowel
-    const matchesCriteria = nor(matches(/^[^aeiou]/), matches(/[aeiou]$/));
-
-    expect(matchesCriteria('wore')).toBe(false);
-    expect(matchesCriteria('apple')).toBe(false);
-    expect(matchesCriteria('toast')).toBe(false);
-    expect(matchesCriteria('our')).toBe(true);
-});
-
-test('and should return true when both values are true', () => {
-    // starts with consonant and ends with vowel
-    const matchesCriteria = and(matches(/^[^aeiou]/), matches(/[aeiou]$/));
-
-    expect(matchesCriteria('wore')).toBe(true);
-    expect(matchesCriteria('apple')).toBe(false);
-    expect(matchesCriteria('toast')).toBe(false);
-    expect(matchesCriteria('our')).toBe(false);
-});
-
-test('nand should return false when both values are true', () => {
-    // starts with consonant and ends with vowel
-    const matchesCriteria = nand(matches(/^[^aeiou]/), matches(/[aeiou]$/));
-
-    expect(matchesCriteria('wore')).toBe(false);
-    expect(matchesCriteria('apple')).toBe(true);
-    expect(matchesCriteria('toast')).toBe(true);
-    expect(matchesCriteria('our')).toBe(true);
-});
-
-test('xor should only return true when one value is true', () => {
-    // starts with consonant and ends with vowel
-    const matchesCriteria = xor(matches(/^[^aeiou]/), matches(/[aeiou]$/));
-
-    expect(matchesCriteria('wore')).toBe(false);
-    expect(matchesCriteria('apple')).toBe(true);
-    expect(matchesCriteria('toast')).toBe(true);
-    expect(matchesCriteria('our')).toBe(false);
-})
